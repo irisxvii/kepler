a4454b5a4d39c604b9597a55927802b712e1e43d
iris <iriskurien@gmail.com>
2025-01-03 17:26:48 +0530

    add paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Details from './pages/Details'
+import Explorer from './pages/Explorer'
+import Home from './pages/Home'
+
 function App() {
 
-  return ( 
-    <>
-    
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/explorer' element={<Explorer />}/>
+        <Route path='/details' element={<Details />}/>
+      </Routes>
+    </Router>
   )
 }
 
